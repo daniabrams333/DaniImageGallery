38ec2ea86c9f7f4f6b2ebe606d11fe2064e810cc
Danielle Abrams <daniabrams333@gmail.com>
2023-10-31 01:26:39 -0400

    Initial Commit

---
+++ DaniImageGallery/DaniImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaniImageGalleryApp.swift
+//  DaniImageGallery
+//
+//  Created by Danielle Abrams on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct DaniImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
